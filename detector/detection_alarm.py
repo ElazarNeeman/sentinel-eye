f29410f7e8c01b1f882da040dc81c31647734030
Elazar Neeman <elazar.neeman@gmail.com>
2024-06-21 20:02:37 +0300

    handled telegram bidirectional communication set telegram keys in env variables

--- detector/detection_alarm.py
+++ detector/detection_alarm.py
@@ -1,61 +1,85 @@
 import asyncio
-import queue
 import threading
 import time
 
 import cv2
-from telethon import TelegramClient
+from telethon import TelegramClient, events
 
 from detection_aggragate import DetectionAggregate
-from telegram import channel_id
+from telegram import TELEGRAM_CHANNEL_ID, TELEGRAM_API_ID, TELEGRAM_API_HASH
 
 
 class DetectionAlarm:
 
-    def __init__(self, client:  TelegramClient):
+    def __init__(self):
         self.agg = DetectionAggregate()
-        self.client = client
+        self.client = None
         self.alarm_time = {}
-        self.alarm_queue = queue.Queue()
-        self.alarm_thread = threading.Thread(target=self.alarm_worker)
+        self.alarm_queue = asyncio.Queue()
+        self.alarm_thread = threading.Thread(target=self.__alarm_thread, args=())
         self.alarm_thread.daemon = True
+        self.files = {}
+
+    def __alarm_thread(self):
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        self.client = TelegramClient('new_session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH)
+        self.client.start()
+
+        @self.client.on(events.NewMessage)
+        async def on_message(event):
+            print("!!! got ", event.raw_text)
+            if 'hello' in event.raw_text:
+                await event.reply('hi!')
+            if 'who' in event.raw_text:
+                print('got who')
+                await self.__reply_who(event)
+            if 'reset' in event.raw_text:
+                await self.__reset_detections(event)
+
+        self.client.loop.run_until_complete(self.__alarm_worker())
+        self.client.loop.close()
+
+    async def start(self):
         self.alarm_thread.start()
 
-    def alarm_worker(self):
-        asyncio.set_event_loop(asyncio.new_event_loop())
+    async def __alarm_worker(self):
+        print("Alarm worker started")
 
-        if self.client:
-            self.client.start()
-            self.client.send_message(channel_id, 'Hello, a-eye!')
+        welcome_message = ('Hello, a-eye! I am ready to raise alarms. send me "who" to get people in house, '
+                           'and "reset" to reset detections')
+
+        print("telegram channel:", TELEGRAM_CHANNEL_ID)
+        await self.client.send_message(TELEGRAM_CHANNEL_ID, welcome_message)
 
         while True:
-            alarm_data, face = self.alarm_queue.get()
+            alarm_data, face = await self.alarm_queue.get()
             if alarm_data is None:
                 break
-            self.process_alarm(alarm_data, face)
+            await self.process_alarm(alarm_data, face)
             self.alarm_queue.task_done()
 
-    def process_alarm(self, alarm_data, face):
+    async def process_alarm(self, alarm_data, face):
         name = alarm_data['name']
         print(f"{time.ctime()} Raising alarm for {alarm_data}")
         alarm_time = time.time()
         file_name = f'./alarms/Users-{round(alarm_time)}.jpg'
         cv2.imwrite(file_name, face)
+        self.files[name] = file_name
 
         if self.client:
-            self.client.send_file(channel_id, file_name,
-                                  caption=f'Person {name} detected at {time.ctime()}, info: {alarm_data}')
+            await self.client.send_file(TELEGRAM_CHANNEL_ID, file_name,
+                                        caption=f'Person {name} detected at {time.ctime()}, info: {alarm_data}')
 
         self.alarm_time[name] = alarm_time
 
-    def raise_alarm(self, alarm_data, face):
-        self.alarm_queue.put((alarm_data, face))
+    async def raise_alarm(self, alarm_data, face):
+        await self.alarm_queue.put((alarm_data, face))
 
     def stop(self):
         self.alarm_queue.put((None, None))
         self.alarm_thread.join()
 
-    def add_detection(self, da: DetectionAggregate, min_frames=5, last_seen_minutes=15):
+    async def add_detection(self, da: DetectionAggregate, min_frames=5, last_seen_minutes=15):
         detections = da.detections
 
         for name in detections:
@@ -63,11 +87,29 @@ class DetectionAlarm:
                 continue
 
             if name not in self.agg.detections:
-                self.raise_alarm({'name': name, **detections[name], }, da.faces[name])
+                await self.raise_alarm({'name': name, **detections[name], }, da.faces[name])
                 continue
 
             name_status = self.agg.detections[name]
             if time.time() > name_status['last_frame_time_epoch'] + last_seen_minutes * 60:
-                self.raise_alarm({'name': name, **detections[name], }, da.faces[name])
+                await self.raise_alarm({'name': name, **detections[name], }, da.faces[name])
 
         self.agg = DetectionAggregate.combine(self.agg, da)
+
+    async def __reply_who(self, _):
+
+        for name, detection in self.agg.detections.items():
+            file_name = self.files.get(name)
+            message = f'{name}\n' \
+                      f'seen first at {time.ctime(detection["first_frame_time_epoch"])}\n' \
+                      f'last at {time.ctime(detection["last_frame_time_epoch"])}\n' \
+                      f'emotions: {detection["emotions"]}\n'
+
+            if file_name is not None:
+                await self.client.send_file(TELEGRAM_CHANNEL_ID, file_name, caption=message)
+            else:
+                await self.client.send_message(TELEGRAM_CHANNEL_ID, message=message)
+
+    async def __reset_detections(self, event):
+        self.agg = DetectionAggregate()
+        await event.reply('done')
--- detector/telegram.py
+++ detector/telegram.py
@@ -1,18 +1,8 @@
-from telethon import TelegramClient, events, sync
+import os
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
-api_id = 123456
-api_hash = '123546789abcdef'
+TELEGRAM_API_ID = os.getenv("TELEGRAM_APP_ID")  # 123456
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")  # '8a09818d578d4950bd26a475f765b44e'
+TELEGRAM_CHANNEL_ID = int(os.getenv("TELEGRAM_CHANNEL_ID"))  # -1001485389244
 
-channel_id = -1002201490384
-# client = TelegramClient('session_name', api_id, api_hash)
-# client.start()
-
-client = None
-
-#client.send_message('+972545664107', 'Hello! Talking to you from Telethon')
-# name = "Elazar"
-# alarm_data = {}
-# file_name = "alarms/Users-1715251227.jpg"
-# client.send_file('+972545664107',file_name, caption=f'Person {name} detected at {time.ctime()}, info: {alarm_data}')
